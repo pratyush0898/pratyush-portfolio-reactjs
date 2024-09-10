a0e06a06ba6c09c63deaf46ce5a185c217a7138c
Pratyush0898 <priyankarangwa@gmail.com>
2024-09-10 15:46:05 +0530

    Update projects

--- src/data/user.js
+++ src/data/user.js
@@ -17,7 +17,7 @@ const INFO = {
 
 	homepage: {
 		title: "Full-stack web, and Astrology enthusiast.",
-		description: 
+		description:
 			"I am a Web developer with expertise in JavaScript Node.js. I have experience in building scalable, secure and reliable web applications using various frameworks and technologies. I enjoy solving complex problems and learning new skills. I am passionate about creating high-quality code that follows best practices and industry standards. I am always looking for new challenges and opportunities to grow as a developer.",
 	},
 
@@ -40,7 +40,7 @@ const INFO = {
 				"Portfolio website of Pratyush Kumar, a talented web developer from India. Explore his skills, projects, and contact him for collaborations.",
 			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
 			linkText: "View Project",
-			link: "https://github.com/pratyush0898/pratyush-portfolio.git",	
+			link: "https://github.com/pratyush0898/pratyush-portfolio.git",
 		},
 		{
 			title: "Web Scraping Tool",
@@ -48,7 +48,7 @@ const INFO = {
 				"Python web scraping tool efficiently extracts data from websites. Automate tasks, gather info, and organize it for your needs.",
 			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/python/python.png",
 			linkText: "View Project",
-			link: "https://github.com/pratyush0898/Web-Scraper.git",	
+			link: "https://github.com/pratyush0898/Web-Scraper.git",
 		},
 		{
 			title: "101 H1ML",
@@ -56,7 +56,7 @@ const INFO = {
 				"101 HTML features 101 project using HTML, CSS, JavaScript, designed to help you learn and master web devlopment basics.",
 			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/html/html.png",
 			linkText: "View Project",
-			link: "https://github.com/pratyush0898/101-html.git",	
+			link: "https://github.com/pratyush0898/101-html.git",
 		},
 		{
 			title: "E-commerce Store",
@@ -64,7 +64,7 @@ const INFO = {
 				"Build a user-friendly online store with product browsing, cart management, and checkout (optional: payment integration).",
 			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
 			linkText: "View Project",
-			link: "https://github.com/pratyush0898/ecommerce-store.git",	
+			link: "https://github.com/pratyush0898/ecommerce-store.git",
 		},
 		{
 			title: "Real-time Chat App",
@@ -72,16 +72,36 @@ const INFO = {
 				"Develop a chat application enabling real-time messaging, chat room creation, and user presence (optional: user authentication).",
 			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
 			linkText: "View Project",
-			link: "/#",	
- 		},
-		 {
-			"title": "OpenAI API Project",
-			"description": "Leverage the power of OpenAI's GPT-3.5 API to create an AI chatbot in Python that runs directly in the terminal.",
-			"logo": "https://static.cdnlogo.com/logos/c/38/ChatGPT.svg",
-			"linkText": "View Project",
-			"link": "https://github.com/pratyush0898/Ai-using-openAI.git"
+			link: "/#",
+		},
+		{
+			title: "OpenAI API Project",
+			description: "Leverage the power of OpenAI's GPT-3.5 API to create an AI chatbot in Python that runs directly in the terminal.",
+			logo: "https://static.cdnlogo.com/logos/c/38/ChatGPT.svg",
+			linkText: "View Project",
+			link: "https://github.com/pratyush0898/Ai-using-openAI.git"
+		},
+		{
+			title: "ArtVision Gallery",
+			description: "A Pinterest-like project built with a front-end focus using array methods (no backend).",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://github.com/pratyush0898/ArtVision-Gallery.git"
+		},
+		{
+			title: "GitHub Theme Toggler Website",
+			description: "A simple website with Dark, Light, and Solarize themes that persist using localStorage.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://github.com/pratyush0898/GitHub-Theme-Toggle-Website"
+		},
+		{
+			title: "Noi World",
+			description: "A dynamic website showcasing the Nio ES6, a luxury electric SUV, created as a personal project using React.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://noiworld.netlify.app"
 		}
-		
 	],
 };
 
